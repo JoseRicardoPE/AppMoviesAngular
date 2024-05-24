6098cee88cbffb564e93843aea32484cd1f9675a
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2024-05-23 19:10:49 -0500

    add image default when not found image from movies API and convert px to rem in styles css

--- src/app/services/movies/movies.service.ts
+++ src/app/services/movies/movies.service.ts
@@ -12,6 +12,7 @@ export class MoviesService {
   private URL = 'https://api.themoviedb.org/3';
   private apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZjI5NTE1YzZhYTMwYzg5MTQxZDM2ZmIyNWFmOTQyNiIsInN1YiI6IjYyOWY2NDIzODUwMDVkMDA1MmI5NmM5YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-Db8x8Pn59YG6Wh0b_shbdmtgyzLoDq7Eox_MJqTFPA';
   private apiUrlImage = 'https://image.tmdb.org/t/p/w500';
+  private imageDefault = 'assets/images/default-poster.png';
   private headers = {
     authorization: `Bearer ${this.apiKey}`
   };
@@ -27,6 +28,10 @@ export class MoviesService {
   }
 
   loadImageMovie(posterPath: string): string {
-    return this.apiUrlImage + posterPath;
+    if (!posterPath) {
+      return this.imageDefault;
+    } else {
+      return this.apiUrlImage + posterPath;
+    }
   }
 }
